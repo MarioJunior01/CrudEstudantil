8f1d4eed754f3ce21ee2e8fb9979af917b41b849
MarioJunior01 <mario.juniorifpr@gmail.com>
2024-07-27 17:03:54 -0300

    Criação dos atributos da classe Turma

--- src/turma/Turma.java
+++ src/turma/Turma.java
@@ -1,4 +1,39 @@
 package turma;
 
+import java.time.LocalDate;
+
 public class Turma {
+
+    private String cursoTurma;
+
+    private int codigoTurma;
+
+    private int capacidadeMaxima;
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public void setCursoTurma(String cursoTurma) {
+        this.cursoTurma = cursoTurma;
+    }
+
+    public String getCursoTurma() {
+        return cursoTurma;
+
+    }
+
+    public int getCodigoTurma() {
+        return codigoTurma;
+    }
+
+    public void setCodigoTurma(int codigoTurma) {
+        this.codigoTurma = codigoTurma;
+    }
+
+
 }
